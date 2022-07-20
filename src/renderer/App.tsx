d8b984ebfdec89d289732a3d2898aa2ce7f2f8d4
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-07-20 14:16:47 +0530

    first commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+import '@stream-io/stream-chat-css/dist/css/index.css';
+
+const filters = { type: 'messaging' };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const App = () => {
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    const newClient = new StreamChat('q95x9hkbyd6p');
+
+    const handleConnectionChange = ({ online = false }) => {
+      if (!online) return console.log('connection lost');
+      setClient(newClient);
+    };
+
+    newClient.on('connection.changed', handleConnectionChange);
+
+    newClient.connectUser(
+      {
+        id: 'ron',
+      },
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c'
+    );
+
+    return () => {
+      newClient.off('connection.changed', handleConnectionChange);
+      newClient.disconnectUser().then(() => console.log('connection closed'));
+    };
+  }, []);
+
+  if (!client) return null;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default App;
